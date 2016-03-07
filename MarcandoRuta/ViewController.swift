8ac7ea63d2c59f7b55050f56ae6733eb1633f67f
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-03-06 22:09:25 -0600

    Solo falta que ponga un pin, cada 50 metros.

--- MarcandoRuta/ViewController.swift
+++ MarcandoRuta/ViewController.swift
@@ -25,22 +25,33 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     
     
 //*************Variables
+    
     //location
-    var latitudeInit: CLLocationDegrees! //longitud
-    var longitudeInit:CLLocationDegrees! //latitud
-    var latDelta: CLLocationDegrees! //zoom
-    var longDelta: CLLocationDegrees! //zoom
-    var span: MKCoordinateSpan! // obtiene el zoom
-    var location: CLLocationCoordinate2D! // obtiene la localización
-    var region: MKCoordinateRegion! // obtiene la region con "location" y "span"
-    var newCoordinate: CLLocationCoordinate2D! //nueva coordenada segun donde presione
+    var latitudeInit: CLLocationDegrees!                //longitud inicial
+    var longitudeInit:CLLocationDegrees!                //latitud inicial
+    var locationInit: CLLocation!                       //locación inicial
+    
+    //zoom
+    var latUserDelta: CLLocationDegrees!                //zoom
+    var longUserDelta: CLLocationDegrees!               //zoom
+    var span: MKCoordinateSpan!                         // obtiene el zoom
+    
+    var location: CLLocationCoordinate2D!               // obtiene la localización
+    var region: MKCoordinateRegion!                     // obtiene la region con "location" y "span"
+    
+    var newCoordinate: CLLocationCoordinate2D!          //nueva coordenada segun donde presione
+    
+    //User
     var userLocation: CLLocation!
     var userLatitude: CLLocationDegrees!
     var userLongitude: CLLocationDegrees!
+
     
-    //var locations = [CLLocation]()
+    //locationManajer
     var locationManager = CLLocationManager()
     
+    
+    //distancia
     var distancia = 0.0
     var seconds = 0.0
     var timer = NSTimer()
@@ -51,69 +62,29 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timer.invalidate()
-        
         //locationManager
         locationManager.delegate = self                             //se delegal al viewControllew
         locationManager.desiredAccuracy = kCLLocationAccuracyBest   // la mejor presición
         locationManager.requestWhenInUseAuthorization()             //requerir autorización al estar en uso
         locationManager.startUpdatingLocation()                     //empezar la localización
+        mapa.showsUserLocation = true                               //mostrar usuario
+
+
         
-        /*
-        latitudeInit = +37.33170303
-        longitudeInit = -122.03024001
-        latDelta = 0.01
-        longDelta = 0.01
-        
-        
-        //locacion en el mapa
-        location = CLLocationCoordinate2DMake(latitudeInit, longitudeInit)
-        span = MKCoordinateSpanMake(latDelta, longDelta)
-        region = MKCoordinateRegionMake(location, span)
-        mapa.setRegion(region, animated: true)
-        
-        */
-        mapa.showsUserLocation = true
-        
-        //distancia
-        //locations.removeAll(keepCapacity: false)
+        //Timer
+        timer.invalidate()
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "eachSecond:", userInfo: nil, repeats: true)
         eachSecond(timer)
 
-        
-        /*
-        //annotation
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Lat: \(latitudeInit), lon: \(longitudeInit)"
-        annotation.subtitle = "Start"
-        
-        mapa.addAnnotation(annotation)
-        */
         
         
         //reconocer gesto de presion
         let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
         uilpgr.minimumPressDuration = 2 //tiempo de reconocimiento del gesto en segundos
         mapa.addGestureRecognizer(uilpgr)
-        
+    
         
 
-    }
-//*********Añadir Pin
-    func action(gestureRecognizer: UIGestureRecognizer){
-    
-        let touchPoint = gestureRecognizer.locationInView(self.mapa)//reconoce donde estamos presionando en el view del mapa
-         newCoordinate = mapa.convertPoint(touchPoint, toCoordinateFromView: self.mapa)
-        
-        //annotation
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = newCoordinate
-        annotation.title = "Lat: \(latitudeInit), lon: \(longitudeInit)"
-        annotation.subtitle = " new Start"
-        
-        mapa.addAnnotation(annotation)
-        
     }
     
     
@@ -124,30 +95,22 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
         
         seconds++
         
-        let secondQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: seconds)
-        timeLabel.text = secondQuantity.description
-        let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: distancia)
-        distanceLabel.text = distanceQuantity.description
-        //print(distanceLabel.text!)
-        
-        
-        
-        
-        let paceUnit = HKUnit.secondUnit().unitDividedByUnit(HKUnit.meterUnit())
-        let paceQuantity  = HKQuantity(unit: paceUnit, doubleValue: seconds/distancia)
-        paceLabel.text = paceQuantity.description
-        
-        
         //Aqui vamos sumar las distancias
         if seconds > 1 {
             
+            
             distanciaTotal += Int(round(distancia))
             print(distanciaTotal)
+
         }else{
+            
             distancia = 0.0
+        
         }
         
-        if distanciaTotal >= 50 && distanciaTotal <= 55 {
+
+        
+        if distanciaTotal >= 50 && distanciaTotal <= 54 {
                 
                 //annotation
                 location = CLLocationCoordinate2DMake(userLatitude, userLongitude)
@@ -157,30 +120,29 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
                 annotation.subtitle = "\(distanciaTotal) "
                 
                 mapa.addAnnotation(annotation)
-            
             }
-        
-        
-        
-        
-        
-        
+
     }
+
     
+    
+//
     func locationManager(manager: CLLocationManager, var didUpdateLocations locations: [CLLocation]) {
         
         self.userLocation = locations[0]
-        userLatitude = userLocation.coordinate.latitude
-        userLongitude = userLocation.coordinate.longitude
+        userLatitude = userLocation.coordinate.latitude             //latitud usuario
+        userLongitude = userLocation.coordinate.longitude           //longitud usuario
+        distancia = userLocation.speed                              //velocidad del usuario en bicicleta
+        //print(locations)
         
-        distancia = userLocation.speed
 
-        latDelta = 0.01
-        longDelta = 0.01
-        
+    
         //locacion en el mapa
+        latUserDelta = 0.01                                         //zoom
+        longUserDelta = 0.01                                        //zoom
+        
         location = CLLocationCoordinate2DMake(userLatitude, userLongitude)
-        span = MKCoordinateSpanMake(latDelta, longDelta)
+        span = MKCoordinateSpanMake(latUserDelta, longUserDelta)
         region = MKCoordinateRegionMake(location, span)
         mapa.setRegion(region, animated: true)
     
@@ -193,17 +155,14 @@ class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelega
         switch segmentControl.selectedSegmentIndex{
             
             case 0:
-                mapa.mapType = MKMapType.Standard
+                mapa.mapType = MKMapType.Standard                   //mapaStandard
             case 1:
-                mapa.mapType = MKMapType.Satellite
+                mapa.mapType = MKMapType.Satellite                  //mapaSatelital
             default:
-                mapa.mapType = MKMapType.Hybrid
+                mapa.mapType = MKMapType.Hybrid                     //mapaHibrido
             
         }
     }
-
-    
- 
 
 
 }
